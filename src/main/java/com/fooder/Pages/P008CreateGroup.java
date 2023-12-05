adc278868d0076b026dfac213d01a5da6272a165
karim-twerlo <karim.salem@twerlo.com>
2023-12-05 23:04:07 +0200

    initiate groups branch

---
+++ src/main/java/com/fooder/Pages/P008CreateGroup.java
@@ -0,0 +1,11 @@
+package com.fooder.Pages;
+
+import com.fooder.PageBase.PageBase;
+import org.openqa.selenium.WebDriver;
+
+public class P008CreateGroup extends PageBase {
+    public P008CreateGroup(WebDriver driver) {
+        super(driver);
+    }
+
+}
---
+++ src/test/java/com/fooder/testCases/M008CreateGroupTest.java
@@ -0,0 +1,9 @@
+package com.fooder.testCases;
+
+import com.fooder.Pages.P008CreateGroup;
+import com.fooder.testBase.BaseTest;
+
+public class M008CreateGroupTest extends BaseTest {
+    P008CreateGroup group ;
+
+}
